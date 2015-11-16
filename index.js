710777d2b1f6b771a52aecda8b63aa77d1209ee9
amyjay <amy@foley.org.uk>
2015-11-16 22:53:26 +0000

    trying ridiculously hard to get a good nav bar

---
+++ index.js
@@ -0,0 +1,10 @@
+var mn = $(".main-nav");
+
+$(window).scroll(function() {
+	if( $(this).scrollTop) >250 {
+		mn.addClass("main-nav-scrolled");
+	}
+	else{
+		mn.removeClass("main-nav-scrolled");
+	}
+});
